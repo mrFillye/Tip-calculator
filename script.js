884c9d89c9663a5372b33e5a85d037c4e7a29f7a
mrFillye <58518377+mrFillye@users.noreply.github.com>
2020-11-01 14:52:37 +0300

    fixed some bugs

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ function calculateTip() {
     const serviseQual = document.getElementById("serviseQual").value;
     const numOfPeople = document.getElementById("peopleamt").value;
 
-    if (billAmt === null && numOfPeople == null) {
-        alert('please enter values');
+    if (billAmt === "" && numOfPeople == 0) {
+        alert("enter values")
         return
     }
 
-    if (numOfPeople === "" || numOfPeople <= 1) {
+    if (billAmt === "" || numOfPeople <= 1) {
         numOfPeople = 1;
-        document.getElementById("each").style.display = "none";
+        document.getElementById("each").style.display = "none"
     } else {
-        document.getElementById("each").style.display = "block";
+        document.getElementById("each").style.display = "none"
     }
 
     let total = (billAmt * serviseQual) / numOfPeople;
     total = Math.round(total * 100) / 100;
-    total = total.toFixed(2);
+    total = total.toFixed(2)
 
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = total;
 }
 
 document.getElementById("totalTip").style.display = "none";
-document.getElementById("each").style.display = "none";
+document.getElementById("each").style.display = "none"
 
 document.getElementById("calculate").onclick = () => {
     calculateTip();
